0931dfd19e5c66c3ab261f4de691bbb8676885e1
mark <markpackham1@gmail.com>
2022-05-04 11:03:26 +0100

    add checks to prevent submission

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -24,6 +24,24 @@ const useWordle = (solution) => {
   // handle keyup event & tract current guess
   // if user presses enter, add the new guess
   const handleKeyup = ({ key }) => {
+    if (key === "Enter") {
+      // only add guess if turn less than 5
+      if (turn > 5) {
+        console.log("Game over, 5 guess max");
+        return;
+      }
+      // do not allow duplicate words
+      if (history.includes(currentGuess)) {
+        console.log("You already tried that word.");
+        return;
+      }
+      // check word is 5 chars long
+      if (currentGuess.length !== 5) {
+        console.log("Guess must be 5 letters");
+        return;
+      }
+    }
+
     if (key === "Backspace") {
       setCurrentGuess((prev) => prev.slice(0, -1));
       return;
