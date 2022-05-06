a5fe4f75bcd44944bc3708eba897dde13508675a
mark <markpackham1@gmail.com>
2022-05-06 14:50:41 +0100

    store letters of alphabet in database
    https://youtu.be/v_AUDday9NY?t=143

---
+++ src/components/Keypad.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+export default function Keypad() {
+  const [letters, setLetters] = useState(null);
+
+  return <div>Keypad</div>;
+}
