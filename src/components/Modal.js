a247169cfa0d06ce61f96fdf0f3c5082121cba94
mark <markpackham1@gmail.com>
2022-05-11 14:38:16 +0100

    set response if win or lost game in Modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,18 +1,22 @@
 import React from "react";
 
-export default function Modal() {
+export default function Modal({ isCorrect, turn, solution }) {
   return (
     <div className="modal">
-      <div>
-        <h1>You Won</h1>
-        <p className="solution"></p>
-        <p>You found the solution in this amount of guesses.</p>
-      </div>
-      <div>
-        <h1>Out of turns, you lost.</h1>
-        <p className="solution"></p>
-        <p>Better luck next time.</p>
-      </div>
+      {isCorrect && (
+        <div>
+          <h1>You Won</h1>
+          <p className="solution"></p>
+          <p>You found the solution in {turn} guesses.</p>
+        </div>
+      )}
+      {!isCorrect && (
+        <div>
+          <h1>Out of turns, you lost.</h1>
+          <p className="solution"></p>
+          <p>Better luck next time, the answer was {solution}.</p>
+        </div>
+      )}
     </div>
   );
 }
